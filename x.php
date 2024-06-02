16f8b0eda143ba07ecefe9d9b8be2bfc2472d895
kartikSingh1119 <kartiksinghx.crew@gmail.com>
2024-06-02 10:28:25 +0530

    Create x.php

---
+++ x.php
@@ -0,0 +1,13 @@
+<?php
+function isLeapYear($year) {
+    return (($year % 4 == 0) && ($year % 100 != 0) || ($year % 400 == 0));
+}
+
+$year = 2024; // Change this to the year you want to check
+
+if (isLeapYear($year)) {
+    echo "$year is a leap year.";
+} else {
+    echo "$year is not a leap year.";
+}
+?>
